ba71bfc205b5ea8ff1bf4760f5ce5fdec81e757c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-09-20 02:02:21 +0500

    We've wrapped our <Component />  in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Box, ChakraProvider } from '@chakra-ui/react'
+import TopBar from '../components/TopBar'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider >
+      <TopBar />
+      <Box maxWidth="container.xl" margin="auto">
+        <Component {...pageProps} />
+      </Box>     
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
